c8543965f45fd9bd275eafdc60f9e280bab9a706
Austin-Wise <austin@wisebydesign.io>
2019-03-10 21:52:29 -0400

    Set up env

---
+++ src/pages/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Style.css';
+import Axios from 'axios';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
