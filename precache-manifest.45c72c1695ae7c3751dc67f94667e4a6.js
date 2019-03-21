29453ba0cd426103f7e06e5fcb6e2461541d2440
Austin-Wise <austin@wisebydesign.io>
2019-03-21 10:43:16 -0400

    Updates

---
+++ precache-manifest.45c72c1695ae7c3751dc67f94667e4a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d45496effcebeab6ce6",
+    "url": "/BookNote/static/js/runtime~main.0952fe7f.js"
+  },
+  {
+    "revision": "a6c6ff09708ce811037d",
+    "url": "/BookNote/static/js/main.d636041a.chunk.js"
+  },
+  {
+    "revision": "8fe24be113e990d24a85",
+    "url": "/BookNote/static/js/2.257394ef.chunk.js"
+  },
+  {
+    "revision": "a6c6ff09708ce811037d",
+    "url": "/BookNote/static/css/main.47da004b.chunk.css"
+  },
+  {
+    "revision": "f7f86259191735b3084b0291332b303a",
+    "url": "/BookNote/index.html"
+  }
+];
